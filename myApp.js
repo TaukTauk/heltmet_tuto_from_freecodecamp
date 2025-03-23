518b4bb3e4728efdee99cd988a40b7c9b9a5fcbd
Rick <tauktauk51833@gmail.com>
2025-03-23 16:19:12 +0700

    add nosniff

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ const helmet = require('helmet');
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter()); // use content security policy instead of this 
-
+app.use(helmet.noSniff()); // protect content type override
 
 
 
