e8871f16c06fee1b1918bf64f5d5a28dc670a588
Rick <tauktauk51833@gmail.com>
2025-03-23 17:11:48 +0700

    debug

--- myApp.js
+++ myApp.js
@@ -2,24 +2,35 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
-app.use(helmet.xssFilter()); // use content security policy instead of this 
-app.use(helmet.noSniff()); // protect content type override
-app.use(helmet.ieNoOpen()); // prevent executing the downloaded file in IE
-app.use(helmet.hsts({maxAge: (90 * 60 * 60 * 24) , force: true}));
-app.use(helmet.dnsPrefetchControl()); //compromise the speed and performance
-app.use(helmet.noCache()); // control cache
-app.use(
-	helmet.contentSecurityPolicy({
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.frameguard({action: 'deny'}));
+// app.use(helmet.xssFilter()); // use content security policy instead of this 
+// app.use(helmet.noSniff()); // protect content type override
+// app.use(helmet.ieNoOpen()); // prevent executing the downloaded file in IE
+// app.use(helmet.hsts({maxAge: (90 * 60 * 60 * 24) , force: true}));
+// app.use(helmet.dnsPrefetchControl()); //compromise the speed and performance
+// app.use(helmet.noCache()); // control cache
+// app.use(
+// 	helmet.contentSecurityPolicy({
+// 	  directives: {
+// 		defaultSrc: ["'self'"], // Only allow resources from the same origin
+// 		scriptSrc: ["'self'", 'trusted-cdn.com'] // Allow scripts only from self & trusted source
+// 	  },
+// 	})
+//   );
+
+app.use(helmet({
+	frameguard: {         // configure
+	  action: 'deny'
+	},
+	contentSecurityPolicy: {    // enable and configure
 	  directives: {
-		defaultSrc: ["'self'"], // Only allow resources from the same origin
-		scriptSrc: ["'self'", 'trusted-cdn.com'] // Allow scripts only from self & trusted source
-	  },
-	})
-  );
-
-
+		defaultSrc: ["'self'"],
+		styleSrc: ['style.com'],
+	  }
+	},
+	dnsPrefetchControl: false     // disable
+  }))
 
 
 
